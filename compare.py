4dd35727566895ee6c6ee537bed3a1f15c34b146
Brian <josue77472@gmail.com>
2025-12-21 01:30:04 -0400

    Mejora lógica con elif para igualdad

---
+++ compare.py
@@ -0,0 +1,9 @@
+x = int(input("Ingrese un número :"))
+y = int(input("Ingrese otro número "))
+
+if x < y:
+    print(f"{x} es menor que {y}")
+elif x > y:
+    print(f"{x} es mayor que {y}")
+else:
+    print(f"{x} es igual que {y}")
