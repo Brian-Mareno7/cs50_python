480ef1698ad553e77317aa40dbed46ec51ebdae6
Brian <josue77472@gmail.com>
2025-12-21 13:19:20 -0400

    Modificado con variables más claras

--- lector.py
+++ lector.py
@@ -1,14 +1,15 @@
-f = open("alumnos.txt", "r")
+archivo = open("alumnos.txt", "r") 
 
 conteo_gryffindor = 0
 
-for linea in f:
-    datos = linea.strip()
-    linea = datos.split(" - ")
-    print(linea[1])
-    if linea[1] == "Gryffindor":
+for linea_sucia in archivo:
+    linea_limpia = linea_sucia.strip()
+    lista_datos = linea_limpia.split(" - ")
+
+    casa_actual = lista_datos[1]
+    
+    if casa_actual == "Gryffindor":
         conteo_gryffindor = conteo_gryffindor + 1
 
-
-f.close()
-print(conteo_gryffindor)
+archivo.close()
+print("Total de leones:", conteo_gryffindor)
