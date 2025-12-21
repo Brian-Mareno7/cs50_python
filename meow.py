6fc14d251416cea873a841e678fd0732a42778ee
Brian <josue77472@gmail.com>
2025-12-21 01:40:30 -0400

    Mejora con bucle for

--- meow.py
+++ meow.py
@@ -1,4 +1,2 @@
-i = 0            # 1. Empezamos el contador
-while i < 3:     # 2. Mientras i sea menor que 3...
-    print("miau")   #    (Aquí va tu miau)
-    i = i + 1    # 3. Sumamos 1 a i
+for i in range(3):
+    print("miau")
